799f52100b6687fa1f04026d7c33470f1a8bce62
Le Quan Dat <dongquanpro2004gmail.com>
2024-10-25 23:14:13 +0700

    sua lan eight

--- video8/video8.js
+++ video8/video8.js
@@ -1,9 +1,20 @@
 console.log('Hello world from JS')
 
-let b = {
+let a = {
     name : 'Dat',
     address : 'Dong Thap',
     major : 'IT'
-}
+};
 
-console.log(b)
+//console.log('my name\'s :', a.name)
+
+//let b = ['MU', 'Chelsea', 'Liver']
+//console.log('type of b', typeof b, b)
+//console.log('firt item :',b[0], 'second item :', b[1])
+
+let family = [
+    { name : 'Dat', address : 'Dong Thap', major : 'IT' },
+    { name : 'Nhi', address : 'Vinh Long', major : 'DS' }
+]
+
+console.log(family[0])
