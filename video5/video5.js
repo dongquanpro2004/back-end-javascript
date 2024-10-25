e752e00d6942f3502ec9db8fccc7619d29526ab4
Le Quan Dat <dongquanpro2004gmail.com>
2024-10-25 22:08:58 +0700

    sua lan 8

---
+++ video5/video5.js
@@ -0,0 +1,12 @@
+let obj = {
+    name : 'Dat',
+    address : 'Dong Thap'
+};
+
+let b = 'name'
+
+obj[b] = 'Nhi'
+
+console.log(`My name is`, obj['name'])
+console.log(`I live in `, obj.address)
+
